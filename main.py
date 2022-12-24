02ee9bd34fbf4467159636a28d8cddcffa4ca204
t.nester <t.nester@disoft.dev>
2022-12-24 16:19:35 +0200

    remove db from prod

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-import sqlite3
 import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
@@ -14,44 +13,9 @@ from bot.keyboards import Keyboard
 
 bot = telebot.TeleBot(settings.TOKEN)
 
-conn = sqlite3.connect(settings.DB_NAME, check_same_thread=False)
-cursor = conn.cursor()
-
-
-def create_user(user_id: int, first_name: str, last_name: str, username: str, chat_id: int):
-    cursor.execute('INSERT INTO bot_user (user_id, first_name, last_name, username, chat_id) VALUES (?, ?, ?, ?, ?)',
-                   (user_id, first_name, last_name, username, chat_id))
-    conn.commit()
-
-
-def get_user(user_id: int):
-    cursor.execute('SELECT * FROM bot_user WHERE user_id = ?', (user_id,))
-    return cursor.fetchone()
-
-
-def get_user_group(user_id: int):
-    cursor.execute('SELECT group_id FROM bot_user WHERE user_id = ?', (user_id,))
-    group = cursor.fetchone()
-    if group:
-        return group[0]
-
-def set_user_group(user_id: int, group_id: int):
-    sql_update_query = """UPDATE bot_user SET group = ? WHERE user_id = ?"""
-    data = (group_id, user_id)
-    cursor.execute(sql_update_query, data)
-    conn.commit()
-
 
 @bot.message_handler(content_types=["text"])
 def main_menu(message):
-    user_id = message.from_user.id
-    chat_id = message.chat.id
-    # if user is None:
-    #     first_name = message.from_user.first_name
-    #     last_name = message.from_user.last_name
-    #     username = message.from_user.username
-    #     create_user(user_id=user_id, first_name=first_name, last_name=last_name, username=username, chat_id=chat_id)
-
     if message.text.lower() in ('/menu', '/start'):
         bot.send_message(message.chat.id, 'hello:)', reply_markup=Keyboard.main())
     elif message.text.lower() == '/which_group':
@@ -66,9 +30,6 @@ def main_menu(message):
     elif message.text.lower() == 'zakaz':
         response_text = Command.zakaz_shedule()
         bot.send_message(message.chat.id, response_text, reply_markup=Keyboard.main())
-    elif message.text.lower() == 'me':
-        me = get_user(user_id=2)
-        bot.send_message(message.chat.id, f'your group is {me}')
     else:
         bot.send_message(message.chat.id, 'unknown')
 
